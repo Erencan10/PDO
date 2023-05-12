bb51754b177b888ee950ec9af97bf3f09ebf48f5
Erencan10 <131658679+Erencan10@users.noreply.github.com>
2023-05-12 13:31:05 +0200

    Update index.php

--- pdo/index.php
+++ pdo/index.php
@@ -1,110 +1,100 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-	<title>Gebruikersgegevens formulier</title>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Gegevensformulier</title>
 </head>
 <body>
-	<?php
-	if (isset($_GET['naam']) && isset($_GET['achternaam']) && isset($_GET['leeftijd']) && isset($_GET['adres']) && isset($_GET['email'])) {
-		// Haal de gegevens uit de GET-array
-		$naam = $_GET['naam'];
-		$achternaam = $_GET['achternaam'];
-		$leeftijd = $_GET['leeftijd'];
-		$adres = $_GET['adres'];
-		$email = $_GET['email'];
 
-		// Print de gegevens op een mooie manier
-		echo "<h2>Ingevoerde gegevens:</h2>";
-		echo "<ul>";
-		echo "<li>Naam: " . $naam . "</li>";
-		echo "<li>Achternaam: " . $achternaam . "</li>";
-		echo "<li>Leeftijd: " . $leeftijd . "</li>";
-		echo "<li>Adres: " . $adres . "</li>";
-		echo "<li>Email: " . $email . "</li>";
-		echo "</ul>";
-	}
-	?>
+<form method="GET"><br>
 
-	<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="GET">
-		<label for="naam">Naam:</label>
-		<input type="text" id="naam" name="naam"><br>
+<label for="naam">Naam</label><br>
 
-		<label for="achternaam">Achternaam:</label>
-		<input type="text" id="achternaam" name="achternaam"><br>
+<input type="text" id="naam" name="naam"><br>
 
-		<label for="leeftijd">Leeftijd:</label>
-		<input type="number" id="leeftijd" name="leeftijd"><br>
+<label for="achternaam">Achternaam</label><br>
 
-		<label for="adres">Adres:</label>
-		<input type="text" id="adres" name="adres"><br>
+<input type="text" id="achternaam" name="achternaam"><br>
 
-		<label for="email">Email:</label>
-		<input type="email" id="email" name="email"><br>
+<label for="leeftijd">Leeftijd</label><br>
 
-		<input type="submit" value="Verzenden">
-	</form>
+<input type="number" id="leeftijd" name="leeftijd"><br>
+
+<label for="adres">Adres</label><br>
+
+<input type="text" id="adres" name="adres"><br>
+
+<label for="email">Email</label><br>
+
+<input type="email" id="email" name="email"><br>
+
+<input type="submit" value="Verstuur"><br>
+
+</form>
 
 </body>
 </html>
 
-hierna is POST methode.
 
+
+Hierna POST methode
 
 
 
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-	<title>Gebruikersgegevens formulier</title>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Gegevensformulier</title>
 </head>
 <body>
-	<?php
-	if ($_SERVER["REQUEST_METHOD"] == "POST") {
-		// Haal de gegevens uit de POST-array
-		$naam = $_POST['naam'];
-		$achternaam = $_POST['achternaam'];
-		$leeftijd = $_POST['leeftijd'];
-		$adres = $_POST['adres'];
-		$email = $_POST['email'];
+<form id='Gegevens' action='Gegevens.php' method='post' 
+    accept-charset='UTF-8'>
 
-		// Print de gegevens op een mooie manier
-		echo "<h2>Ingevoerde gegevens:</h2>";
-		echo "<ul>";
-		echo "<li>Naam: " . $naam . "</li>";
-		echo "<li>Achternaam: " . $achternaam . "</li>";
-		echo "<li>Leeftijd: " . $leeftijd . "</li>";
-		echo "<li>Adres: " . $adres . "</li>";
-		echo "<li>Email: " . $email . "</li>";
-		echo "</ul>";
-	}
-	?>
+<fieldset >
 
-	<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="POST">
-		<label for="naam">Naam:</label>
-		<input type="text" id="naam" name="naam"><br>
+<legend>Gegevens</legend><br>
 
-		<label for="achternaam">Achternaam:</label>
-		<input type="text" id="achternaam" name="achternaam"><br>
+<input type='hidden' name='submitted' id='submitted' value='1'/><br>
 
-		<label for="leeftijd">Leeftijd:</label>
-		<input type="number" id="leeftijd" name="leeftijd"><br>
+<label for='naam' >Naam </label><br>
 
-		<label for="adres">Adres:</label>
-		<input type="text" id="adres" name="adres"><br>
+<input type='text' name='naam' id='naam' maxlength="50" /><br>
 
-		<label for="email">Email:</label>
-		<input type="email" id="email" name="email"><br>
+<label for='achternaam' >Achternaam</label><br>
 
-		<input type="submit" value="Verzenden">
-	</form>
+<input type='text' name='achternaam' id='achternaam' maxlength="50" /><br>
 
+<label for='leeftijd' >Leeftijd:</label><br>
+
+<input type='text' name='leeftijd' id='leeftijd' maxlength="50" /><br>
+
+<label for='adres' >Adres</label><br>
+
+<input type='adres' name='adres' id='adres' maxlength="50" /><br>
+
+<input type='submit' name='Submit' value='Submit' /><br>
+
+<label for='email' >Email</label><br>
+
+<input type='text' name='email' id='email' maxlength="50" /><br>
+
+</fieldset>
+</form>
 </body>
 </html>
 
 
 
 
-GET en POST zijn twee efficiënte technieken die de gegevens naar de server kunnen verzenden en de browser heeft deze noodzakelijkerwijs nodig om met de server te communiceren.
+Verschil tussen GET en POST methode:
+
+GET en POST zijn twee efficiënte technieken die de gegevens naar de server kunnen verzenden,
+ en de browser heeft deze noodzakelijkerwijs nodig om met de server te communiceren.
 De twee methoden zijn verschillend waar de GET-methode de gecodeerde gegevens aan de URI toevoegt,
 terwijl in het geval van de POST-methode de gegevens aan het lichaam worden toegevoegd in plaats van aan de URI.
 Bovendien wordt de GET-methode gebruikt voor het ophalen van de gegevens.
